3f03ee63b24156a5c6f104bd81af192878e4750e
Ben Shuttleworth <benshutt2002@gmail.com>
2024-04-22 16:15:14 -0400

    added code for production

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template
 from backend import get_short_form_image_captions
 
@@ -17,6 +18,6 @@ def index():
     return render_template('web_ui.html', captions=captions)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 
