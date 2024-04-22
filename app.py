90329a3ebdadca67de1eaa2fdd3a80aa80804846
Ben Shuttleworth <benshutt2002@gmail.com>
2024-04-22 15:52:59 -0400

    final test version v2

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def index():
         uploaded_file.save('temp.jpg')
         # Call the backend function to process the image
         captions = get_short_form_image_captions("amazing-city-418415", "us-east4", 'temp.jpg')
-        print(captions)
     return render_template('web_ui.html', captions=captions)
 
 if __name__ == '__main__':
